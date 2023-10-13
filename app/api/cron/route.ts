adeea0f585797bebcc1e813aff47478f9f62483c
ADC <aliza.conde@swintt.com>
2023-10-13 16:18:25 +0800

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; //5 minutes
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET() {
     try {
         connectToDB();
@@ -38,7 +42,7 @@ export async function GET() {
                   //Update Products in DB
                   const updatedProduct = await Product.findOneAndUpdate(
                     {
-                        url: scrapedProduct.url
+                        url: product.url
                     },
                     product
                   );
